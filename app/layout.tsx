f007ebb3b1880145e3349a7be233503926fb012e
Oliver Oswald <oliver.oswald25@gmail.com>
2023-02-21 16:00:08 +0100

    added daisy ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,36 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    <>
+      <div className="min-h-full">
+        <nav className="navbar fixed">
+          <div className="flex-1">
+            <a href="/" className="btn btn-ghost normal-case text-xl">
+              Fitness App
+            </a>
+          </div>
+          <div className="flex-none">
+            <div className="dropdown dropdown-end">
+              <a href="/login" className="btn btn-primary">
+                Login
+              </a>
+              <a href="/register" className="btn ml-2 btn-secondary">
+                Register
+              </a>
+            </div>
+          </div>
+        </nav>
+        <div className="py-4">
+          <div className="mx-auto max-w-7xl px-3 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
