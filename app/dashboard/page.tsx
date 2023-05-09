37d3be63101209d2005a450c29fdce37de51a25d
Oliver Oswald <oliver.oswald25@gmail.com>
2023-05-09 20:56:00 +0200

    fixed bmi

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,14 @@
+import Bmi from "@/components/Bmi";
+
 export default function Home() {
   return (
     <div className="min-h-full flex flex-row justify-center items-center">
-      <div className="flex items-center">
+      <div className="flex flex-col items-center">
         <div className="flex justify-center items-center">
-          <h1 className="text-9xl font-bold pt-10">Fitness App</h1>
+          <h1 className="text-9xl font-bold py-10">Fitness App</h1>
         </div>
-              <div className="flex">
-                  
-                  
+        <div className="flex justify-center items-center">
+          <Bmi />
         </div>
       </div>
     </div>
